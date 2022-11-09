6b0574e2ad83fb88346da839f3508e439d324ac8
Kyle Dell <kdell1@umbc.edu>
2022-11-09 13:29:41 -0500

    added data and parsing

--- backend/server.py
+++ backend/server.py
@@ -26,6 +26,46 @@ def landmarks():
     csv_file.close()
     return landmarks
 
+def covid():
+    covid_dates = []
+    with open('./baltimore_city_zip_code_totals.csv') as csv_file:
+        totals = []
+        increments = []
+        for line in csv_file.readlines():
+            totals.append(line.split(',')[1:])
+        
+        for i in range(1, len(totals)):
+            for j in range(1, len(totals[i])):
+                if totals[i][j] == '':
+                    totals[i][j] = 0
+                totals[i][j] = int(totals[i][j])
+        
+        for i in range(1, len(totals[0])):
+            totals[0][i] = totals[0][i].strip('total').strip('\n')
+
+        increments.append(totals[:1])
+        for i in range(len(totals) - 1):
+            increments.append([])
+        for i in range(1, len(totals)):
+            for j in range(len(totals[i])):
+                if j == 0:
+                    increments[i].append(totals[i][0])
+                elif j == 1:
+                    increments[i].append(totals[i][j] - 0)
+                else:
+                    increments[i].append(totals[i][j] - totals[i][j - 1])
+
+        for i in range(1, len(totals[0])):
+            temp = {}
+            temp['date'] = totals[0][i]
+            for j in range(1, len(totals)):
+                temp[totals[j][0]] = {
+                    'total' : totals[j][i],
+                    'increment' : increments[j][i]
+                }
+            covid_dates.append(temp)
+        return covid_dates
+
 def index(endpoint):
     if request.method == "GET":
         if endpoint == "":
@@ -33,6 +73,8 @@ def index(endpoint):
         elif endpoint == "landmarks":
             # temp function until database is created
             return landmarks()
+        elif endpoint == "covid":
+            return covid()
         else:
             return endpoint
 
